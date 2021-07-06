bd1fb402a52355225496180423c703aab013788c
durian <a2091013@outlook.com>
2021-07-06 20:29:40 +0800

    Update update.sh
    删除掉docker部分和chinadns部分，只专注于smartdns gfwlist的更新

--- default/update.sh
+++ default/update.sh
@@ -20,14 +20,3 @@ cat /tmp/temp_gfwlist | sed 's/^/\./g' > /tmp/smartdns_gfw_domain.conf
 sed -i 's/^/nameserver \//' /tmp/smartdns_gfw_domain.conf
 sed -i 's/$/\/GFW/' /tmp/smartdns_gfw_domain.conf
 cat default/gfw_group.conf /tmp/smartdns_gfw_domain.conf > smartdns_gfw_domain.conf
-
-# Update Overture GFW List
-
-cp -f /tmp/temp_gfwlist docker/overture/overture_gfw_domain.list
-
-# Update China List
-
-curl -sSL https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt > /tmp/china_ipv4.list
-curl -sSL https://bgp.space/china6.html | grep "^[0-9a-z:\/]*<br>" | sed "s/<br>//g" > /tmp/china_ipv6.list
-echo > /tmp/line
-cat /tmp/china_ipv4.list /tmp/line /tmp/china_ipv6.list > docker/overture/china_ip.list
